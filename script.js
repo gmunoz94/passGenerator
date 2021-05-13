7379388638450994d84227b38c68e123c85dc084
gmunoz94 <g.munozii@icloud.com>
2021-05-12 22:19:02 -0500

    Wrote function to convert password base into password, not working currently

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
-// Arrays for Password Generator
-var capital = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-var lower = ['a', 'b', 'c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-var number = [1,2,3,4,5,6,7,8,9,0]
-var special = ['!','"','#','$','%','&',"'",'(',')','*','+',',','-','.',':',';','<','=','>','?','@','[',']','^','_','`','{','}','|','~']
-
-
 // Need to prompt user with choices of 'Password Length' and the ability to select if Special Characters, Numbers, Lower Case, and Upper case letters are desired. 
 // Need to tailor function depending on user input
 // Need to choose random item from random array (within chosen selections)
 // Need to make sure at least one item from each array is chosen (within chosen selections)
 // Need to display password on webpage
-
-var capRand = capital[Math.floor(Math.random()*capital.length)];
-var lowRand = lower[Math.floor(Math.random()*lower.length)];
-var numRand = number[Math.floor(Math.random()*number.length)];
-var specRand = special[Math.floor(Math.random()*special.length)];
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -25,27 +13,36 @@ var criteriaBtn = document.querySelector("#charSelect");
 
 
 // Functions
+// Getting the Password Length
 function submitLength() {
   passLength = document.getElementById("passLength").value;
+  // Logs password Length (for checking funtionality)
   console.log(passLength);
+  // Checks to make sure length is between 8-128 characters
   if(passLength <= 7 || passLength >= 129) {
     alert("Invalid Entry! Please input a number between 8-128");
     return;
   } 
+  // Hides the Password Length Prompt
   document.getElementById("card1").style.visibility = "hidden";
+  // Shows Criteria Prompt
   document.getElementById("card2").style.visibility = "visible";
 };
 
 function sumbitCriteria () {
+  // Checks if each criteria selection is Chosen
   upper = document.getElementById("upperCrit").checked;
   lower = document.getElementById("lowerCrit").checked;
   numVal = document.getElementById("numCrit").checked;
   specVal = document.getElementById("specCrit").checked;
+  // Logs password Criteria Selection (for checking funtionality)
   console.log(upper);
   console.log(lower)
   console.log(numVal);
   console.log(specVal);
+  // Hides Criteria Prompt
   document.getElementById("card2").style.visibility = "hidden";
+  // Multiple checks to see which criteria is chosen and creates an array accordingly
   if (upper === true && lower === true && numVal === true && specVal === true) {
      passArray = [0,1,2,3]
     console.log(passArray);
@@ -65,8 +62,9 @@ function sumbitCriteria () {
   }
 };
 
+// Declare array to put base of password in
+var basePass = [];
 function getBase() {
-  var basePass = [];
   for (var i = 0; i < passLength; i++) {
     passLengthArray = passArray[Math.floor(Math.random()*passArray.length)];
     console.log(passLengthArray);
@@ -75,13 +73,35 @@ function getBase() {
     console.log(basePass);
 }
 
-function getPass() {
-  
-  for (var i = 0; i < passLength; i++) {
-    if(i == 0) {
+// Arrays for Password Generator
+capital = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+lower = ['a', 'b', 'c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+number = [1,2,3,4,5,6,7,8,9,0]
+special = ['!','"','#','$','%','&',"'",'(',')','*','+',',','-','.',':',';','<','=','>','?','@','[',']','^','_','`','{','}','|','~']
 
+// Declare Array to put password in
+var newPass = [];
+function getPass() {
+  for (var i = 0; i < passLength; i++) {
+    if (basePass == 0) {
+      capRand = capital[Math.floor(Math.random()*capital.length)];
+      console.log(capRand);
+      newPass.push(capRand);
+    } else if (basePass == 1) {
+      lowRand = lower[Math.floor(Math.random()*lower.length)];
+      console.log(lowRand);
+      newPass.push(lowRand);
+    } else if (basePass == 2) {
+      numRand = number[Math.floor(Math.random()*number.length)];
+      console.log(numRand);
+      newPass.push(numRand);
+    } else if (basePass == 3) {
+      specRand = special[Math.floor(Math.random()*special.length)];
+      console.log(specRand);
+      newPass.push(specRand);
     }
   }
+  console.log(newPass);
 }
 
 // Write password to the #password input
